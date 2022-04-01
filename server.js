b5041ef1ccd8589ead3cefeaa7bbe97b613a9013
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-01 15:18:28 -0700

    structure

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
